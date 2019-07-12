a046100866ca7d1b2461b6ff62eb2f341b506966
Administator <gitlab@zealfi.com>
2019-07-12 12:02:51 +0800

    midblur

--- python学习/medianblur.py
+++ python学习/medianblur.py
@@ -16,8 +16,8 @@ import numpy as np
 
 img = cv2.imread('d:/pictures/mid.jpg',0)
     
-median = cv2.medianBlur(img, 5)
-compare = np.concatenate((img, median), axis=1)
+img_medianblur = cv2.medianBlur(img, 5)
+compare = np.concatenate((img, img_medianblur), axis=1)
 
 cv2.imshow('img', compare)
 key = cv2.waitKey()
@@ -28,22 +28,26 @@ if key == 27:
 def medianBlur(img, kernel, padding_way):
     W,H = img.shape
     m,n = kernel.shape 
-    window = np.zeros(m,n)
-    img_median = np.zeros(W,H)
+    img_median = np.zeros((W+m-1,H+n-1))
     if padding_way == 'ZERO':
-        img_pad = np.pad(img,((m-1,m-1),(n-1,n-1)),'constant')
+        img_pad = np.pad(img,((m-1,m-1),(n-1,n-1)),'constant') #元祖1增加行，元祖2增加列
     if padding_way == 'REPLICA':
         img_pad = np.pad(img,((m-1,m-1),(n-1,n-1)),'edge')
-    for x in range(W):
-        for y in range(H):
-            for i in range(m):
-                for j in range(n):
-                    window[i,j] = img_pad[x,y]*kernel[0,0]
-            img_median[x,y] = np.median(window)
-        
+    for x in range(W+m-1):
+        for y in range(H+n-1):
+            window = img_pad[x:x+m,y:y+n]*kernel
+            img_median[x,y] = np.median(window)        
     return img_median
 
 
-kernel = np.ones((3,2))
+kernel = np.ones((7,4))
 A = medianBlur(img,kernel,'ZERO')
 B = medianBlur(img,kernel,'REPLICA')
+
+
+cv2.imshow('img',A)
+key = cv2.waitKey()
+if key == 27:
+    cv2.destroyAllWindows()
+
+
