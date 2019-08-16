c4d1a034368fb6b0db10fc5977e39949521f0599
Administator <gitlab@zealfi.com>
2019-08-16 14:26:25 +0800

    用多种模型训练minst数据集

--- torch学习/minst_卷积神经网络.py
+++ torch学习/minst_卷积神经网络.py
@@ -1,102 +1,133 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Sat Aug  3 16:32:55 2019
-
-@author: chenxiahang
+类LeNet5的卷积神经网络能达到99%的准确率了
 """
 
 import pickle
 import gzip
 from pathlib import Path
 import torch
-from torch import nn
-import torch.nn.functional as F
-from torch import optim
-from torch.utils.data import TensorDataset
 from torch.utils.data import DataLoader
-import numpy as np
+from torch.utils.data import TensorDataset
+from torch import nn
 
+
+#读取数据
 DATA_PATH = Path('data')
 PATH = DATA_PATH / "mnist"
 FILENAME = "mnist.pkl.gz"
 with gzip.open((PATH / FILENAME).as_posix(),"rb") as f:
 	((x_train,y_train),(x_valid,y_valid),(x_test,y_test)) = pickle.load(f,encoding="latin-1")
-    
+
 x_train,y_train,x_valid,y_valid = map(torch.tensor,(x_train,y_train,x_valid,y_valid))
-n,c = x_train.shape
 
-class Mnist_Logistic(nn.Module):
-	def __init__(self):
-		super().__init__()
-		self.lin = nn.Linear(784,10)
-
-	def forward(self,xb):
-		return self.lin(xb)
-
-def get_model():
-	model = Mnist_Logistic()
-	return model, optim.SGD(model.parameters(),lr=lr)
-
-def accuracy(out,yb):
-	preds = torch.argmax(out,dim=1)           # 得到最大值的索引
-	return (preds == yb).float().mean()
-
-
-lr = 0.5  # learning rate
-epoches = 20  # how many epochs to train for
-bs = 64 					# batch size
-xb = x_train[0:bs] 
-yb = y_train[0:bs]
-loss_func = F.cross_entropy
+bs_train = 64
+bs_valid = 128
 train_ds = TensorDataset(x_train,y_train)
-valid_ds = TensorDataset(x_valid, y_valid)
+train_dl = DataLoader(train_ds, batch_size=bs_train,shuffle=True)
+valid_ds = TensorDataset(x_valid,y_valid)
+valid_dl = DataLoader(valid_ds, batch_size=bs_valid,shuffle=True)
 
-def get_data(train_ds, valid_ds, bs):
-    return (
-        DataLoader(train_ds, batch_size=bs, shuffle=True),
-        DataLoader(valid_ds, batch_size=bs * 2),
-    )
 
-def loss_batch(model, loss_func, xb , yb, opt=None):
-	loss = loss_func(model(xb),yb)
+#定类似于LeNet5的网络
+class MinstCnn(nn.Module):
+    def __init__(self):
+        super(MinstCnn, self).__init__()
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(1, 25, kernel_size=5), #(25,24,24)
+            nn.BatchNorm2d(25),
+            nn.ReLU(inplace=True)
+        )
+ 
+        self.layer2 = nn.Sequential(
+            nn.MaxPool2d(kernel_size=2, stride=2) #(25,12,12)
+        )
+ 
+        self.layer3 = nn.Sequential(
+            nn.Conv2d(25, 50, kernel_size=3),
+            nn.BatchNorm2d(50),
+            nn.ReLU(inplace=True)  #(50,10,10)
+        )
+ 
+        self.layer4 = nn.Sequential(
+            nn.MaxPool2d(kernel_size=2, stride=2) #(50,5,5,)
+        )
+ 
+        self.fc = nn.Sequential(
+            nn.Linear(50 * 5 * 5, 1024),
+            nn.ReLU(inplace=True),
+            nn.Linear(1024, 128),
+            nn.ReLU(inplace=True),
+            nn.Linear(128, 10)
+        )
+ 
+    def forward(self, x):
+        x = x.view(-1,1,28,28)
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+        return x
 
-	if opt is not None:
-		loss.backward()
-		opt.step()
-		opt.zero_grad()
-	
-	return loss.item(), len(xb)
+net = MinstCnn()
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(net.parameters(),lr=0.05,momentum = 0.9)
 
-def fit(epochs, model, loss_func, opt, train_dl, valid_dl):
-    for epoch in range(epoches):
-        model.train()
-        for xb,yb in train_dl:
-            loss_batch(model, loss_func, xb, yb, opt)
-        model.eval()
-        with torch.no_grad():
-            losses, nums = zip(*[loss_batch(model,loss_func,xb,yb) for xb, yb in valid_dl])
-        val_loss = np.sum(np.multiply(losses,nums))/np.sum(nums)
-        print(epoch, val_loss)
 
-class Mnist_CNN(nn.Module):
-	def __init__(self):
-		super().__init__()
-		self.conv1 = nn.Conv2d(1,16,kernel_size=3,stride=2,padding=1)  
-		self.conv2 = nn.Conv2d(16,16,kernel_size=3,stride=2,padding=1)
-		self.conv3 = nn.Conv2d(16,10,kernel_size=3,stride=2,padding=1)
+losses = []
+acces = []
+eval_losses = []
+eval_acces = []
 
-	def forward(self, xb):
-		xb = xb.view(-1,1,28,28)
-		xb = F.relu(self.conv1(xb))
-		xb = F.relu(self.conv2(xb))
-		xb = F.relu(self.conv3(xb))
-		xb = F.avg_pool2d(xb,4)
-		return xb.view(-1, xb.size(1))
+xb,yb = iter(train_dl).next()
+for e in range(10):
+    train_loss = 0
+    train_acc = 0
+    net.train()
+    for xb, yb in train_dl:
+        # 前向传播
+        out = net(xb)
+        loss = criterion(out, yb)
+        # 反向传播
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        # 记录误差
+        train_loss += loss.item()
+        # 计算分类的准确率
+        _, pred = out.max(1)
+        num_correct = (pred == yb).sum().item()
+        acc = num_correct / xb.shape[0]
+        train_acc += acc
+        
+    losses.append(train_loss / len(train_dl)) #每个epoch循环完做一次平均
+    acces.append(train_acc / len(train_dl))  #每个epoch循环完做一次平均
+    
+    # 在测试集上检验效果
+    eval_loss = 0
+    eval_acc = 0
+    net.eval() # 将模型改为预测模式
+    for im, label in valid_dl:
+        out = net(im)
+        loss = criterion(out, label)
+        # 记录误差
+        eval_loss += loss.item()  #测试集上不用反向传播这一步
+        # 记录准确率
+        _, pred = out.max(1)  #行方向的最值
+        num_correct = (pred == label).sum().item()
+        acc = num_correct / im.shape[0]
+        eval_acc += acc
+        
+    eval_losses.append(eval_loss / len(valid_dl))
+    eval_acces.append(eval_acc / len(valid_dl))
+    print('epoch: {}, Train Loss: {:.6f}, Train Acc: {:.6f}, Eval Loss: {:.6f}, Eval Acc: {:.6f}'
+          .format(e, train_loss / len(train_dl), train_acc / len(train_dl), 
+                     eval_loss / len(valid_dl), eval_acc / len(valid_dl)))
 
-lr=0.1
-train_dl, valid_dl = get_data(train_ds, valid_ds, bs)
-model, opt = get_model()
-fit(epoches, model, loss_func, opt, train_dl, valid_dl)
-            
+
+
+
 
